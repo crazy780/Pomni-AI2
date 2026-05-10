1ca3b013d40dd620201222d2211e0636be536e95
crazy780 <maghurwurturkya@gmail.com>
2026-05-10 22:48:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '967776798110', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
